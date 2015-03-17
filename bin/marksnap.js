aefe4bd6a4ccac698b90c3f3824921db28dd8171
AaronJan <aaronjan@qq.com>
2015-03-17 11:13:58 +0800

    update description

--- bin/marksnap.js
+++ bin/marksnap.js
@@ -1,6 +1,18 @@
 #!/usr/bin/env node
 'use strict';
 
+/**
+ * Marksnap
+ * ----------------------------------------------------------------------------
+ * Marksnap is a CLI tool for parse markdown(.md) to HTML, PDF.
+ * ----------------------------------------------------------------------------
+ * @version 0.9
+ * @author  AaronJan <https://github.com/AaronJan>
+ * @link    https://github.com/AaronJan/marksnap
+ * @license http://ww.apache.org/licenses/LICENSE-2.0 The Apache License V2
+ * ----------------------------------------------------------------------------
+ */
+
 var argv = require('minimist')(process.argv.slice(2));
 var helper = require('../lib/helper');
 var converter = require('../lib/converter');
