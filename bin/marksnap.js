7034d2c2985f17925d503bcc441b660794c8e6d0
AaronJan <aaronjan@qq.com>
2015-09-22 18:52:09 +0800

    update

--- bin/marksnap.js
+++ bin/marksnap.js
@@ -7,15 +7,18 @@ var helper   = require('../lib/helper.js');
 var marksnap = require('../lib/core.js');
 
 
-
-
-
 try {
   if (argv._.length != 0 && ! argv.h && ! argv.help) {
     /**
      * Generate converting options and convert.
      */
-    marksnap.startFromCLI(argv, process.cwd());
+    marksnap.startFromCLI(argv, process.cwd())
+      .then(function (job) {
+        console.log('convert success!');
+      })
+      .catch(function (e) {
+        throw e;
+      });
   } else {
     helper.showHelp();
   }
